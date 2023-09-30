6bbe8752c133c3dd24dcf1bcb784f01229c324f9
Artemii <zakhar@DESKTOP-ECRNMH1.localdomain>
2023-09-30 04:27:45 +0300

    p-c

--- mlops/model.py
+++ mlops/model.py
@@ -1,14 +1,15 @@
-from sklearn.pipeline import Pipeline
-from sklearn.linear_model import LinearRegression
-from sklearn import datasets
 from joblib import load
+from sklearn import datasets
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import Pipeline
 
 data = datasets.load_diabetes()
 
-model = Pipeline(steps=[
-#        ('preprocessor', preprocessor),
-        ('linearregression', LinearRegression())
-        ])
+model = Pipeline(
+    steps=[
+        #        ('preprocessor', preprocessor),
+        ("linearregression", LinearRegression())
+    ]
+)
 
-model.fit(data['data'], data['target'])
-
+model.fit(data["data"], data["target"])
