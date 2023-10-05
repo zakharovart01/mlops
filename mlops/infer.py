cf559863285587262541e4626d0b9b12c430f349
Artemii <zakhar@DESKTOP-ECRNMH1.localdomain>
2023-10-05 04:12:00 +0300

    train infer

---
+++ mlops/infer.py
@@ -0,0 +1,22 @@
+import logging
+from joblib import load
+from sklearn import datasets
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+
+    df = datasets.load_diabetes()
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        df['data'], df['target'], test_size=0.33, random_state=42
+    )
+
+    model = load("../models/1.joblib")
+
+    model_score = model.score(X_test, y_test)
+
+    logging.info(f"model score: {model_score:.3f}")
