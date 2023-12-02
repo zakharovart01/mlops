07019a768a808829e3d34ef4371843b388b429eb
Artemii <zakhar@DESKTOP-ECRNMH1.localdomain>
2023-12-02 23:37:11 +0300

    3

--- mlops/infer.py
+++ mlops/infer.py
@@ -1,34 +1,51 @@
 import logging
-import dvc.api
+
+import gspread
+import hydra
+import numpy as np
 import pandas as pd
-from dvc.api import DVCFileSystem
-
 from joblib import load
-from sklearn import datasets
+from oauth2client.service_account import ServiceAccountCredentials
 from sklearn.model_selection import train_test_split
-from io import StringIO
 
-if __name__ == "__main__":
+
+@hydra.main(config_path="../conf", config_name="config", version_base="1.3")
+def main(cfg):
     logging.basicConfig(level=logging.DEBUG)
 
-    # 13a8UvVIFkZRKzcEmOAA-tSI4weA5xYaQ
-    # df = datasets.load_diabetes()
-    #https://docs.google.com/spreadsheets/d/1l3EBkmx0Mb5qHDLNiMwDKw6KlwvJyGpMP8V9GGbnCOE/edit#gid=397432644
-    # with dvc.api.open_url('dvc+gdrive://13a8UvVIFkZRKzcEmOAA-tSI4weA5xYaQ/data.csv') as f:
-    #     data = pd.read_csv(f)
-    # data = dvc.api.get_url(path='data.csv', repo='dvc+gdrive://13a8UvVIFkZRKzcEmOAA-tSI4weA5xYaQ/data.csv', remote='myremote')
-    # with dvc.api.open('gdrive://13a8UvVIFkZRKzcEmOAA-tSI4weA5xYaQ/data.csv', remote='myremote') as f:
-    #     data = pd.read_csv(f)
+    scope = [
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/drive",
+    ]
+    c = ServiceAccountCredentials.from_json_keyfile_name(
+        "../token.\
+    json",
+        scope,
+    )
+    client = gspread.authorize(c)
+    sheet_id = "1l3EBkmx0Mb5qHDLNiMwDKw6KlwvJyGpMP8V9GGbnCOE"
+    sheet = client.open_by_key(sheet_id)
+    tab_name = "data.csv"
+    worksheet = sheet.worksheet(tab_name)
+    data = pd.DataFrame(worksheet.get_all_values())
+    logging.info("--------------------------------------")
+    logging.info(data.columns)
+    logging.info("--------------------------------------")
+    data.columns = data.iloc[0]
+    data = data.iloc[1:]
+    logging.info("--------------------------------------")
+    logging.info(data.shape)
+    logging.info("--------------------------------------")
+    logging.info(data.columns)
+    logging.info("--------------------------------------")
+    logging.info(cfg)
+    logging.info("--------------------------------------")
 
-    # with dvc.api.open('data/data.csv') as fd:
-    #     data = pd.read_csv(fd)
-    with dvc.api.open('gdrive://13a8UvVIFkZRKzcEmOAA-tSI4weA5xYaQ/data.csv', remote='gdrive') as fd:
-        data = pd.read_csv(fd)
-    target = data['target']
-    data = data.drop(columns=['target'], inplace=True)
+    target = np.array(data["target"])
+    data = np.array(data[[str(i) for i in range(10)]])
 
     X_train, X_test, y_train, y_test = train_test_split(
-        data, target, test_size=0.33, random_state=42
+        data, target, test_size=cfg.d.t_s, random_state=cfg.d.r_s
     )
 
     model = load("1.joblib")
@@ -37,6 +54,13 @@ if __name__ == "__main__":
 
     model_score = model.score(X_test, y_test)
 
-    # logging.info(f"model score: {model_score:.3f}")
-    print("model_score =", model_score)
+    logging.info(f"model score: {model_score:.3f}")
+    # print("model_score =", model_score)
+    # если что все логи выше помогли мне отдебажить код
+    # чтение данных из гугл диска взял так как я делал
+    # ибо dvc api чтобы бархлил и будто только с репозитория качать может
     pred.tofile("pred.csv", sep="")
+
+
+if __name__ == "__main__":
+    main()
--- mlops/train.py
+++ mlops/train.py
@@ -1,18 +1,53 @@
 import logging
 
+import gspread
+import hydra
+import numpy as np
+import pandas as pd
 from joblib import dump
-from sklearn import datasets
+from oauth2client.service_account import ServiceAccountCredentials
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 
-if __name__ == "__main__":
+
+@hydra.main(config_path="../conf", config_name="config", version_base="1.3")
+def main(cfg):
     logging.basicConfig(level=logging.DEBUG)
 
-    df = datasets.load_diabetes()
+    scope = [
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/drive",
+    ]
+    c = ServiceAccountCredentials.from_json_keyfile_name(
+        "../token.\
+    json",
+        scope,
+    )
+    client = gspread.authorize(c)
+    sheet_id = "1l3EBkmx0Mb5qHDLNiMwDKw6KlwvJyGpMP8V9GGbnCOE"
+    sheet = client.open_by_key(sheet_id)
+    tab_name = "data.csv"
+    worksheet = sheet.worksheet(tab_name)
+    data = pd.DataFrame(worksheet.get_all_values())
+    logging.info("--------------------------------------")
+    logging.info(data.columns)
+    logging.info("--------------------------------------")
+    data.columns = data.iloc[0]
+    data = data.iloc[1:]
+    logging.info("--------------------------------------")
+    logging.info(data.shape)
+    logging.info("--------------------------------------")
+    logging.info(data.columns)
+    logging.info("--------------------------------------")
+    logging.info(cfg)
+    logging.info("--------------------------------------")
+
+    target = np.array(data["target"])
+    data = np.array(data[[str(i) for i in range(10)]])
 
     X_train, X_test, y_train, y_test = train_test_split(
-        df["data"], df["target"], test_size=0.33, random_state=42
+        data, target, test_size=cfg.d.t_s, random_state=cfg.d.r_s
     )
 
     model = Pipeline(
@@ -29,3 +64,7 @@ if __name__ == "__main__":
     # logging.info(f"model score: {model_score:.3f}")
 
     dump(model, "1.joblib")
+
+
+if __name__ == "__main__":
+    main()
