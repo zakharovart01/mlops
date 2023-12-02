46b77be029b87dd48f16ce1fb84d384c85aabadf
Artemii <zakhar@DESKTOP-ECRNMH1.localdomain>
2023-12-02 18:22:05 +0300

    Add data from Google Sheets

--- mlops/infer.py
+++ mlops/infer.py
@@ -1,4 +1,6 @@
 import logging
+import dvc.api
+import pandas as pd
 
 from joblib import load
 from sklearn import datasets
@@ -8,9 +10,14 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
 
     df = datasets.load_diabetes()
+    #https://docs.google.com/spreadsheets/d/1l3EBkmx0Mb5qHDLNiMwDKw6KlwvJyGpMP8V9GGbnCOE/edit#gid=397432644
+    with dvc.api.open_url('https://docs.google.com/spreadsheets/d/1l3EBkmx0Mb5qHDLNiMwDKw6KlwvJyGpMP8V9GGbnCOE/edit#gid=397432644') as f:
+        data = pd.read_csv(f)
+        target = data['target']
+        data = data.drop(columns=['target'], inplace=True)
 
     X_train, X_test, y_train, y_test = train_test_split(
-        df["data"], df["target"], test_size=0.33, random_state=42
+        data, target, test_size=0.33, random_state=42
     )
 
     model = load("1.joblib")
@@ -20,5 +27,5 @@ if __name__ == "__main__":
     model_score = model.score(X_test, y_test)
 
     # logging.info(f"model score: {model_score:.3f}")
-    print('model_score =',model_score)
-    pred.tofile('pred.csv', sep = '')
+    print("model_score =", model_score)
+    pred.tofile("pred.csv", sep="")
